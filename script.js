22542583380232fa440d15f3722076c0233e064b
Karthik Arun R <karthikarun2000@gmail.com>
2024-12-04 16:50:47 +0530

    Initial rough version

---
+++ script.js
@@ -0,0 +1,17 @@
+// Basic JavaScript for interactivity
+window.onload = function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    function changeActiveLink() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    changeActiveLink();
+    window.addEventListener('scroll', changeActiveLink);
+};
